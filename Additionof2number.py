19ca17fc0ce0ec8b37d7c730cda22e8ce25e6ad0
MonishPro <mayekarmonish875@gmail.com>
2022-12-02 20:21:19 +0530

    Commit

---
+++ Additionof2number.py
@@ -0,0 +1,10 @@
+# This was calculation of two numbers using int data type
+
+num1=int(input("Enter number 1: "))
+num2=int(input("Enter number 2: "))
+
+print("The result after addition of two numbers is :",num1+num2)
+print("The result after subtraction of two numbers is :",num1-num2)
+print("The result after multiplication of two numbers is :",num1*num2)
+print("The result after division of two numbers is :",num1/num2)
+
