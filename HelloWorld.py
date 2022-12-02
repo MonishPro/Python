288fe8100b66c61947fd68ff9a1941559b646cc2
MonishPro <mayekarmonish875@gmail.com>
2022-12-02 19:51:25 +0530

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,11 @@
+a=int(input("Enter the value for number 1:"))
+b=int(input("Enter the value for number 2:"))
+c=int(input("Enter the value for number 3:"))
+if(a>b):
+ print(a+"is greater than"+b+"and"+c)
+
+elif(b>a):
+    print(b"is greater than"+a+"and"+c)
+
+else:
+    print("Error")
---
+++ PrintsandComments.py
@@ -0,0 +1,10 @@
+print("Hello Monish")
+print("Hello Soham")
+print("Hello Sumeet")
+print()
+# This is ths usage of new Line
+# This is the usage of Comments
+
+print("Hello Monish\n")
+print("Hello Soham\n")
+print("Hello Sumeet\n")
