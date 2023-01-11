7b528654ecda2cbed87be21d6872f01739fb1a0a
Monish <112893052+MonishPro@users.noreply.github.com>
2023-01-11 17:55:47 +0530

    functions

---
+++ functions with maths.py
@@ -0,0 +1,14 @@
+import math
+
+# Many such Math functions exists, suit yourself with the needs
+# Following code also explains the use of functions
+
+number=int(input("Enter a number :"))
+
+def first():
+    print("pie :",math.pi*number)
+    print("pow :",math.pow(number,2))
+    print("square root :",math.sqrt(number))
+    print("Radians :",math.radians(number))
+
+first()
